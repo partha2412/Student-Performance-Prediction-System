c9b11406d824d5ff028dfa5ba9b3406241ccb486
Partha Singh <parthasingh35@gmail.com>
2026-01-21 21:43:46 +0530

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='bg-red-500'>
+        <h1 className="text-4xl font-bold text-center mt-10">Welcome to the Student Performance Prediction System</h1>
+    </div>
+  )
+}
+
+export default HomePage
